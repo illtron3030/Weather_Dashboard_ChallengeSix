6b493fa8ea43a2f4ad02a9da160ab1e23bee0ba7
illtron3030 <illtron3030@gmail.com>
2022-08-14 13:16:21 -0700

    not how i want to spend my Sundays

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -45,5 +45,57 @@ function loadWeatherZip(zipCode, isClicked) {
             saveLocations(response);
             renderLocations();
         }
-    }
+    
+    
+    
+        getWeatherData(response.city.coord.lat, response.city.coord.lon, response.city.name);
+    
+    }).catch(function (response){
+        alert("Not a vaild Zip Code")
+    });
+}
+
+function loadWeatherCity(city, isClicked) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=" + APIKey;
+    var weatherContainer = $("#weatherContainer");
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function (response) {
+
+
+
+        if (!isClicked)
+        {
+            saveLocations(response);
+            renderLocations();
+        }
+
+
+        getWeatherData(response.city.coord.lat, response.city.coord.lon, response.city.name);
+
+    }).catch(function(response){
+        alert("Not a valid City");
+    });
+}
+
+function showWeatherData(weatherData, city)
+{
+
+    var iconURL = "http://openweathermap.org/img/w/" + weatherData.current.weather[0].icon + ".png";
+    $("#cityDate").html(city + " (" + new Date().toLocaleDateString() + ") <img id=\"icon\" src=\"" + iconURL  + "\" alt=\"Weather icon\"/>");
+
+    var temp = parseInt(weatherData.current.temp);
+    temp = Math.round(((temp-273.15)*1.8) + 32);
+    $("#currentTemp").html(" " + temp +  "  &degF");
+    $("#currentHumidity").html(weatherData.current.humidity + "%");
+    $("#currentWindSpeed").html(weatherData.current.wind_speed + " MPH");
+
+
+    var uvIndex = weatherData.current.uvi;
 }
