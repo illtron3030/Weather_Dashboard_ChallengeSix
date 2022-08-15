df8b40810cc7fa707a43b962035f475763d79b52
illtron3030 <illtron3030@gmail.com>
2022-08-14 20:34:30 -0700

    post tutor session, huge help getting it all to work

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,129 +1,218 @@
 let APIKey = "6d28ee9d4a6dba4952c75a12c9046c96";
 let loactions = [];
 
+function getWeatherData(lat, lon, city) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=,minutely,hourly,alerts&appid=" +
+    APIKey;
 
-
-
-function getWeatherData(lat, lon, city){
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=,minutely,hourly,alerts&appid=" + APIKey;
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-        .then(function(response){
-
-
-
-            showWeatherData(reponse, city)
-        
-        });
-};
-
-
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+  .then(function (response) {
+    showWeatherData(response, city);
+  });
+}
 
 function loadWeatherZip(zipCode, isClicked) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?zip=" +
+    zipCode +
+    ",us&appid=" +
+    APIKey;
+  var weatherContainer = $("#weatherContainer");
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&appid=" + APIKey;
-    var weatherContainer = $("#weatherContainer");
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
 
+    .then(function (response) {
+      //console.log(response);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+      if (!isClicked) {
+        saveLocations(response);
+
+        renderLocations();
+      }
+
+      getWeatherData(
+        response.city.coord.lat,
+        response.city.coord.lon,
+        response.city.name
+      );
     })
-
-    .then(function(response)){
-
-
-
-        if(!isClicked)
-        {
-            saveLocations(response);
-            renderLocations();
-        }
-    
-    
-    
-        getWeatherData(response.city.coord.lat, response.city.coord.lon, response.city.name);
-    
-    }).catch(function (response){
-        alert("Not a vaild Zip Code")
+    .catch(function (response) {
+      alert("Not a vaild Zip Code");
     });
 }
 
 function loadWeatherCity(city, isClicked) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    ",us&appid=" +
+    APIKey;
+  var weatherContainer = $("#weatherContainer");
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=" + APIKey;
-    var weatherContainer = $("#weatherContainer");
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
 
     .then(function (response) {
+      if (!isClicked) {
+        saveLocations(response);
+        renderLocations();
+      }
 
-
-
-        if (!isClicked)
-        {
-            saveLocations(response);
-            renderLocations();
-        }
-
-
-        getWeatherData(response.city.coord.lat, response.city.coord.lon, response.city.name);
-
-    }).catch(function(response){
-        alert("Not a valid City");
+      getWeatherData(
+        response.city.coord.lat,
+        response.city.coord.lon,
+        response.city.name
+      );
+    })
+    .catch(function (response) {
+      alert("Not a valid City");
     });
 }
 
-function showWeatherData(weatherData, city)
-{
+function showWeatherData(weatherData, city) {
+  var iconURL =
+    "http://openweathermap.org/img/w/" +
+    weatherData.current.weather[0].icon +
+    ".png";
+  $("#cityDate").html(
+    city +
+      " (" +
+      new Date().toLocaleDateString() +
+      ') <img id="icon" src="' +
+      iconURL +
+      '" alt="Weather icon"/>'
+  );
 
-    var iconURL = "http://openweathermap.org/img/w/" + weatherData.current.weather[0].icon + ".png";
-    $("#cityDate").html(city + " (" + new Date().toLocaleDateString() + ") <img id=\"icon\" src=\"" + iconURL  + "\" alt=\"Weather icon\"/>");
+  var temp = parseInt(weatherData.current.temp);
+  temp = Math.round((temp - 273.15) * 1.8 + 32);
+  $("#currentTemp").html(" " + temp + "  &degF");
+  $("#currentHumidity").html(weatherData.current.humidity + "%");
+  $("#currentWindSpeed").html(weatherData.current.wind_speed + " MPH");
 
-    var temp = parseInt(weatherData.current.temp);
-    temp = Math.round(((temp-273.15)*1.8) + 32);
-    $("#currentTemp").html(" " + temp +  "  &degF");
-    $("#currentHumidity").html(weatherData.current.humidity + "%");
-    $("#currentWindSpeed").html(weatherData.current.wind_speed + " MPH");
+  var uvIndex = weatherData.current.uvi;
 
+  var bgColor = "";
+  var textColor = "";
 
-    var uvIndex = weatherData.current.uvi;
+  if (uvIndex < 3) {
+    bgColor = "bg-good";
+    textColor = "text-light";
+  } else if (uvIndex > 2 && uvIndex < 6) {
+    bgColor = "bg-warning";
+    textColor = "text-dark";
+  } else {
+    bgColor = "bg-danger";
+    textColor = "text-light";
+  }
 
-    var bgColor = "";
-    var textColor = "";
-    
-    if (uvIndex < 3)
-    {
-        bgColor = "bg-good";
-        textColor = "text-light";
-    }
-    else if (uvIndex > 2 && uvIndex < 6)
-    {
-        bgColor = "bg-warning";
-        textColor = "text-dark";
-    }
-    else
-    {
-        bgColor = "bg-danger";
-        textColor = "text-light";
-    }
+  $("#currentUVIndex")
+    .html(uvIndex)
+    .addClass(bgColor + " p-1 " + textColor);
 
-    $("#currentUVIndex").html(uvIndex).addClass(bgColor + " p-1 " +  textColor);
+  var ul5 = $("#fiveDay");
+  ul5.empty();
 
+  for (i = 1; i < 6; i++) {
+    var div = $("<div>").addClass("bg-primary");
 
+    var dateTime = parseInt(weatherData.daily[i].dt);
+    var dateHeading = $("<h6>").text(
+      new Date(dateTime * 1000).toLocaleDateString()
+    ); //convert unix time to javascript date
+    var iconDayURL =
+      "http://openweathermap.org/img/w/" +
+      weatherData.daily[i].weather[0].icon +
+      ".png"; //get weather icon
+    var icon = $("<img>").attr("src", iconDayURL);
 
-    var ul5 = $("#fiveDay");
-    ul5.empty();
+    temp = parseInt(weatherData.daily[i].temp.day);
+    temp = Math.round((temp - 273.15) * 1.8 + 32);
+    var temp5 = $("<p>").html("Temp: " + temp + "  &degF");
 
-    for (i=1; i < 6; i++)
+    var humidity5 = $("<p>").html(
+      "Humidity: " + weatherData.daily[i].humidity + "%"
+    );
+
+    div.append(dateHeading);
+    div.append(icon);
+    div.append(temp5);
+    div.append(humidity5);
+    ul5.append(div);
+  }
+
+  $("#weatherData").show();
 }
+
+function loadLocations() {
+  var locationsArray = localStorage.getItem("locations");
+  if (locationsArray) {
+    locations = JSON.parse(locationsArray);
+    renderLocations();
+  } else {
+    localStorage.setItem("locations", JSON.stringify(locations));
+  }
+}
+
+function renderLocations() {
+  var divLocations = $("#locationHistory");
+  //divLocations.empty();
+
+  $.each(locations, function (index, item) {
+    var a = $("<a>")
+      .addClass("list-group-item list0group-item-action-city")
+      .attr("data-city", locations[index])
+      .text(locations[index]);
+    divLocations.append(a);
+  });
+
+  $("#locationHistory > a").off();
+
+  $("#locationHistory > a").click(function (event) {
+    var element = event.target;
+    var city = $(element).attr("data-city");
+
+    loadWeatherCity(city, true);
+  });
+  locations = [];
+}
+
+function saveLocations(data) {
+  var city = data.city.name;
+
+  locations.unshift(city);
+  localStorage.setItem("locations", JSON.stringify(locations));
+}
+
+$(document).ready(function () {
+  $("#weatherData").hide();
+
+  loadLocations();
+
+  $("#searchBtn").click(function (event) {
+    var element = event.target;
+    var searchCriteria = $("#zipCode").val();
+
+    if (searchCriteria !== "") {
+      var zip = parseInt(searchCriteria);
+
+      if (!isNaN(zip)) {
+        loadWeatherZip(zip, false);
+      } else {
+        loadWeatherCity(searchCriteria, false);
+      }
+    }
+  });
+});
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Weather_Dashboard_ChallengeSix
+
+![badge](https://img.shields.io/github/last-commit/illtron3030/Weather_Dashboard_ChallengeSix)
+
 Weather Dashboard for Challenge #6
+
+    askndaksndjknasjkdnjkasndjkn
+    dsafasfwefwedfwefwefwef
